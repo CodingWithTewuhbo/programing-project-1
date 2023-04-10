cc0a6e267f5297ef3efcf48b4f0113fd3837c91f
CodingWithTewuhbo <130156544+CodingWithTewuhbo@users.noreply.github.com>
2023-04-10 13:17:37 +0300

    Add files via upload

---
+++ poject at class.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main() {
+int i,j,k,b;
+for(i=1;i<=10;i++)
+{
+
+
+  if((i==1)||(i==2)||(i==3)){
+k=1;
+do{
+cout<<"*"<<" ";
+k++;}while(k<=8);
+}
+else
+for(j=1;j<=10-i+1;j++){
+cout<<"*"<<" ";
+}
+if(i==2)
+cout<<"  ";
+for(int n=1;n<=2*(i-2);n++){
+cout<<"  ";
+b=n;}
+for(int m=1;m<=11-i;m++){
+cout<<"*"<<" ";
+}
+cout<<endl;
+
+
+}
+//for(int z=1;z<=9;z++){
+//	if((z==8)||(z==9)){
+//	int a=1;
+//	do{
+//		//for(int a=1;a<=8;a++)
+//		cout<<"*"<<" ";
+//	}while(a<=8);}
+//	 else
+//	for(int x=1;x<=z+1;x++){
+//		cout<<"*"<<" ";
+//	}
+//	for(int c=b;c>=1;c=b-2){
+//		cout<<" ";
+//	}
+//	for(int d=1;d<=z+1;d++){
+//		cout<<"*"<<" ";
+//	}
+//	
+//}
+
+    
+   return 0;}
